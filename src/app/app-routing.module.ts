906a5f7fd8a4963418bb52f58bcba5928716672a
SamarRaboudi <raboudisamar25@gmail.com>
2023-03-05 20:44:29 +0100

    creation of the petFormPage

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { SignupComponent } from './signup/signup.component';
+import { ChoosePetComponent } from './choose-pet/choose-pet.component';
 
 const routes: Routes = [
   {path: '', component: LoginComponent},
   {path: 'signup', component: SignupComponent}, 
+  {path: 'choosePet', component: ChoosePetComponent}, 
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { SignupComponent } from './signup/signup.component';
+import { ChoosePetComponent } from './choose-pet/choose-pet.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    SignupComponent
+    SignupComponent,
+    ChoosePetComponent
   ],
   imports: [
     BrowserModule,
